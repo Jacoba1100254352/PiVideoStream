e1decf2e10eca65f29c641bc2b0662b606bf3f3e
Jacob Anderson <jacobdanderson@gmail.com>
2023-10-29 15:08:54 -0600

    zoomStream v.3

--- zoomStream.py
+++ zoomStream.py
@@ -1,18 +1,23 @@
 # For Pi
-import av
+import io
+import socket
+import struct
+import time
 import picamera
 
-container = av.open("rtmp://10.37.68.171/live/test")
+# Create a socket connection between the Raspberry Pi and the computer
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('10.37.68.171', 8000))
+connection = client_socket.makefile('wb')
 
-with picamera.PiCamera() as camera:
-    camera.resolution = (640, 480)
-    stream = container.add_stream('vp9', rate=30)
-    stream.width = camera.resolution[0]
-    stream.height = camera.resolution[1]
-
-    camera.start_recording(stream, format='vp9')
-    while True:
-        camera.wait_recording(1)
-
-camera.stop_recording()
-container.close()
+try:
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)
+        # Start a stream to the socket
+        camera.start_recording(connection, format='h264')
+        while True:
+            camera.wait_recording(1)
+finally:
+    camera.stop_recording()
+    connection.close()
+    client_socket.close()
